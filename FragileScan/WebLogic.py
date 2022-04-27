a2cffd7cd9aa6ddeebcb27f20255290517448890
ErosionZhu <erosionzhu@outlook.com>
2022-04-27 17:47:01 +0800

    Web logic控制端检测更名

--- FragileScan/WebLogic.py
+++ FragileScan/WebLogic.py
@@ -2,7 +2,7 @@ import requests
 import re
 import socket
 import time
-from POC import CheckLive
+from POC import WebLogicConsole
 from POC import CVE_2014_4210
 from POC import CVE_2016_0638
 from POC import CVE_2016_3510
@@ -18,7 +18,7 @@ from POC import CVE_2019_2729
 from POC import CVE_2019_2890
 from POC import CVE_2020_14882
 
-POC_LIST = ['CheckLive', 'CVE_2014_4210', 'CVE_2016_0638', 'CVE_2016_3510', 'CVE_2017_3428', 'CVE_2017_3506',
+POC_LIST = ['WebLogicConsole', 'CVE_2014_4210', 'CVE_2016_0638', 'CVE_2016_3510', 'CVE_2017_3428', 'CVE_2017_3506',
             'CVE_2017_10271', 'CVE_2018_2628', 'CVE_2018_2893', 'CVE_2018_2894', 'CVE_2018_3191', 'CVE_2019_2725',
             'CVE_2019_2729', 'CVE_2019_2890', 'CVE_2020_14882']
 
